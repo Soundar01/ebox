8d46198cb9544ce417a946397faadbedaa7b0daa
Soundar S <59559538+Soundar01@users.noreply.github.com>
2021-10-01 16:53:44 +0530

    Add files via upload
    Pythagorean Triplet of a+b+c=1000.

---
+++ PythagoreanTriplet.java
@@ -0,0 +1,16 @@
+package hcl.com.ebox;
+
+public class PythagoreanTriplet {
+	 public static void main(String[] args) {
+	        
+		 for (int a = 1; a < 1000; a++) {
+				for (int b = a + 1; b < 1000; b++) {
+					int c = 1000 - a - b;
+					if (a * a + b * b == c * c) {
+						System.out.println("Pythogorean Triplet of a+b+c=1000 is:"+a*b*c);
+					}
+				}
+			}
+	    }
+
+}
