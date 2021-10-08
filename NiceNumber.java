fb42f2a7963f3d4c80b6527d45b08fdd4d3535e0
Soundar S <59559538+Soundar01@users.noreply.github.com>
2021-10-08 18:23:24 +0530

    Add files via upload

---
+++ NiceNumber.java
@@ -0,0 +1,26 @@
+package hcl.com.ebox;
+import java.util.*;
+public class NiceNumber {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		int count=0;
+		for(int i=1;i<=num;i++)
+		{
+			if(num%i==0)
+			{
+				count++;
+			}
+		}
+		if(count==4)
+		{
+			System.out.println("Nice");
+		}
+		else
+		{
+			System.out.println("Not Nice");
+		}
+	}
+
+}
