d3361c3c076064e66f5022a78935c5ebe717ccb7
Soundar S <59559538+Soundar01@users.noreply.github.com>
2021-10-15 16:46:09 +0530

    Add files via upload

---
+++ Euler14.java
@@ -0,0 +1,29 @@
+package hcl.com.ebox;
+
+public class Euler14 {
+
+	public static void main(String[] args) {
+		 int max = 1;
+		    int ans = 1;
+		    for (int i = 2; i < 1000000; i++) {
+		        long n = i;
+		        int t = 0;
+		        do {
+		            ++t;
+		            if (n % 2 == 0) {
+		                n /= 2;
+		            } else {
+		                n = 3 * n + 1;
+		            }
+		        } while (n != 1);
+		        if (t > max) {
+		            max = t;
+		            ans = i;
+		        }
+		    }
+		    System.out.println("Longest chain: "+ans);
+		}
+
+	
+
+}
