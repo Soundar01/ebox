fde284938045f0f87528ec8babcb495cc81b5114
Soundar S <59559538+Soundar01@users.noreply.github.com>
2021-10-15 18:00:26 +0530

    Add files via upload

---
+++ CollectionMainOne.java
@@ -0,0 +1,20 @@
+package Collection;
+import java.util.*;
+public class CollectionMainOne {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int i=0;
+		ArrayList<String> alist=new ArrayList<String>();
+		do {
+			i++;
+			System.out.println("name: "+i);
+			String name=sc.next();
+			alist.add(name);
+			System.out.println("Enter choice y/n:");
+			//char choice=sc.next().charAt(0);
+		}while(sc.next().equalsIgnoreCase("y"));
+		sc.close();
+		alist.forEach((a)->System.out.println("Names:"+a));
+	}
+}
